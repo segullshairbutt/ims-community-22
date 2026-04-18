9d01147e0e3d1f9a1c6acef20b4eaefcad16a47c
gulshairbutt <segullshairbutt@gmail.com>
2026-04-18 09:10:12 +0200

    Adds the github actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ims-community-22/',
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src'),
